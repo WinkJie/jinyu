10116b3f985c9d34826e57409e3aa2ce05acf055
WinkJie <295213085@qq.com>
2023-06-03 23:53:06 +0800

    new

--- js/baidumap.js
+++ js/baidumap.js
@@ -9,7 +9,7 @@ window.onload = function() {
     alert(script.src);
 
     // 创建地图并设置中心点和缩放级别
-    var map = new BMap.Map("map-container");
+    var map = new BMap.Map("map");
     var point = new BMap.Point(116.404, 39.915);
     map.centerAndZoom(point, 15);
 
@@ -20,34 +20,5 @@ window.onload = function() {
 
 //
 //
-// // 初始化地图对象  这里的id必须对应页面中HTML元素的id
-//     var map = new BMap.Map("map-container");
-// // 创建地理编码实例
-//     var myGeo = new BMap.Geocoder();
-// // 地址（城市名+详细地址）
-//     var address = "山西省临汾市襄汾县南贾镇荀董村";
-// // 将地址解析结果显示在地图上，并调整地图视野
-//     myGeo.getPoint(address, function(point){
-//         if (point) {
-//             // 将地图中心点移动到目标位置
-//             map.panTo(point);
-//             // 在该位置创建标注
-//             var marker = new BMap.Marker(point);
-//             map.addOverlay(marker);
-//
-//             // 获取该位置的详细信息
-//             var infoWindow = new BMap.InfoWindow("");
-//             myGeo.getLocation(point, function(result){
-//                 infoWindow.setContent(result.address);
-//                 marker.openInfoWindow(infoWindow);
-//             });
-//
-//             map.centerAndZoom(point, 15);
-// // 添加缩放和平移控件
-//             map.addControl(new BMap.NavigationControl());
-//             map.addControl(new BMap.ScaleControl());
-//         } else {
-//             alert("您选择地址没有解析到结果!");
-//         }
-//     }, "北京市");
+
 };
